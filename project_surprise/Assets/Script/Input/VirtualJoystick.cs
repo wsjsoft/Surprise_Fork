75357e4d01f346e1b0a80eae78c5a9177b7a6b50
Nova-B <46260481+Nova-B@users.noreply.github.com>
2022-10-12 18:20:00 +0900

    Change VirtualJoysitck_Beom
    캔버스 스케일러 0 -> 0.5(height, width)

--- project_surprise/Assets/Script/Input/VirtualJoystick.cs
+++ project_surprise/Assets/Script/Input/VirtualJoystick.cs
@@ -3,62 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems; // 키보드, 마우스, 터치를 이벤트로 오브젝트에 보낼 수 있는 기능 지원
 
-public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     [SerializeField]
     private RectTransform lever;
     private RectTransform rectTransform; // 조이스틱의 RectTransform
 
+    [HideInInspector]
+    public Vector3 Dir { get; private set; }
+
     [SerializeField, Range(10, 150)] // Range()이 범위 안에서만 값의 수정이 가능하게!
     private float leverRange;
-
-    private Vector2 inputDirection;
-    private bool isInput;
-
-    private PlayerInput input;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
-        input = FindObjectOfType<PlayerInput>();
-    }
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        ControlJoystickLever(eventData);
-        isInput = true;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        ControlJoystickLever(eventData);
+        lever.position = eventData.position;
+        lever.localPosition = Vector3.ClampMagnitude(eventData.position - (Vector2)transform.position, rectTransform.rect.width * 0.5f);
+
+        Dir = new Vector3(lever.localPosition.x, 0, lever.localPosition.y).normalized;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        lever.anchoredPosition = Vector2.zero; //손을 떼면 조이스틱의 중심으로 돌아가게
-        isInput = false;
-        input.Movement(Vector2.zero);
-    }
-
-    private void ControlJoystickLever(PointerEventData eventData)
-    {
-        var inputPos = eventData.position - rectTransform.anchoredPosition;
-        var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
-        lever.anchoredPosition = inputVector; // inputVector : 해상도를 기반으로 만들어진 값이라 캐릭터움직임에 쓰면 너무 큰 값이라 너무 빠르게 움직일 것임
-        inputDirection = -1*(inputVector / leverRange); // 따라서, 0~1사이의 정규화된 값을 캐릭터 움직임으로 전달하기위해
-        // 캐릭터의 움직임이 조이스틱의 레버 방향과 반대로되서 -1곱함
-    }
-
-    private void InputControlVector() // 캐릭터에게 입력벡터 전달
-    {
-        input.Movement(inputDirection);
-    }
-
-    void Update()
-    {
-        if (isInput)
-        {
-            InputControlVector();
-        }
+        lever.localPosition = Vector3.zero;
+        Dir = Vector3.zero;
     }
 }
