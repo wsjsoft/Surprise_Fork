a4494c40905c448082746c7760a474bbd0c93cf1
Nova-B <seongbeom1115@gmail.com>
2022-10-13 00:43:52 +0900

    PlayerController cam 변경_Beom
    cam이 freelook camera가 아닌 mainCamera여서 이를 변경해줌

--- project_surprise/Assets/Script/PlayerController.cs
+++ project_surprise/Assets/Script/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
     //변경 변수
     CharacterController controller = null;
     VirtualJoystick virtualJoystick = null;
-    Transform cam = null;
+    Camera cam;
 
     Vector3 joystickDirection;
     Vector3 moveDirection;
@@ -47,7 +47,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
             controller = GetComponent<CharacterController>();
             animator = GetComponent<Animator>();
             virtualJoystick = FindObjectOfType<VirtualJoystick>();
-            cam = FindObjectOfType<CinemachineFreeLook>().transform;
+            cam = Camera.main;
         }
 
     }
@@ -83,27 +83,37 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         //변경 후
         
-        if(virtualJoystick != null)
-        {
-            joystickDirection = virtualJoystick.Dir;
+        /*if(virtualJoystick != null)
+        {*/
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-            if(joystickDirection.magnitude>=0.1f)
+            Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f)
             {
-                float targetAngle = cam.eulerAngles.y + Mathf.Atan2(joystickDirection.x, joystickDirection.z) * Mathf.Rad2Deg;
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+                //cam 회전값이 기본인 상태(cam.eulerAngles.y)에서 플레이어 회전 인풋값(Mathf.Atan2(direction.x, direction.z))을 넣는다
+                //AcrTan(x/y)를 의미 -> 유니티 좌표계는 y축 +방향이 0도로 시계방향 좌표계를 쓰기 때문에 y/x가 아닌 x/y
+                //Atan는 절대각을 -π/2 ~ π/2의 라디안 값으로 반환
+                //Atan2는 두 점 사이의 상대좌표(x, y)를 받아 절대각을 -π ~ π의 라디안 값으로 반환 -> 데카르트 좌표에 유용. 음수를 허용
 
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothTime, turnSmoothVelocity);
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                //현재각과 목표 회전값까지 사이의 자연스러운 회전을 위한 각도를 계속해서 계산
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-                moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                isMove = true;
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                //targetangle 방향의 z축 값(앞 방향)만을 취한다
+
+                controller.SimpleMove(moveDir.normalized * speed); //controller.move와 다른 점은 Time.deltaTime를 곱해주지 않아도 됨. 또 지면 방향 설정을 해주면 중력은 자동 계산 해준다
             }
             else
             {
-                moveDirection = Vector3.zero;
-                isMove = false;
-            }
+                Vector3 moveDir = Vector3.zero;
+                //moveDir.y -= 6f * Time.deltaTime;
 
-            controller.SimpleMove(moveDirection.normalized * speed);
-        }
+                controller.SimpleMove(moveDir.normalized * speed); //move
+            }
+        
     }
 }
